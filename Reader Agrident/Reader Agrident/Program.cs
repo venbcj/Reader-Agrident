2baff04ee60106670f6f69a24ecee36042e283e2
venbcj <bas.vandeven@telfort.nl>
2019-11-29 12:41:11 +0100

    Setup serial port reader

---
+++ Reader Agrident/Reader Agrident/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Reader_Agrident
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SerialPort _serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
+			// Set the read/write timeouts
+			_serialPort.ReadTimeout = 500;
+			_serialPort.WriteTimeout = 500;
+
+			_serialPort.Open();
+
+			_serialPort.Write("[CSW]");
+			
+			System.Threading.Thread.Sleep(300);
+
+			while (_serialPort.BytesToRead > 0) {
+				string response = _serialPort.ReadLine();
+
+				Console.WriteLine(response);
+			}
+			
+
+			_serialPort.Close();
+
+		}
+	}
+}
---
+++ Reader Agrident/TestReaderApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestReaderApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			SerialPort _serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
+			// Set the read/write timeouts
+			_serialPort.ReadTimeout = 500;
+			_serialPort.WriteTimeout = 500;
+
+			_serialPort.Open();
+
+			_serialPort.Write("[CSW]");
+			System.Threading.Thread.Sleep(300);
+
+			// Clear the current buffer
+			_serialPort.DiscardInBuffer();
+
+			_serialPort.Write("[CSW]");
+
+			System.Threading.Thread.Sleep(300);
+
+			string response = "";
+			while (_serialPort.BytesToRead > 0)
+			{
+				response += _serialPort.ReadLine();
+			}
+
+			txtBox.Text = response;
+
+			_serialPort.Close();
+		}
+	}
+}
